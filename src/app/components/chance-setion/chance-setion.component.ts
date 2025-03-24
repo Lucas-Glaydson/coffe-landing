c2e832c027445eb415a6786ddd677c94058d0e5f
Lucas Glaydson <lucas.glaydson@jala.university>
2025-03-24 05:23:53 -0300

    feat: crate feedback section

---
+++ src/app/components/chance-setion/chance-setion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chance-setion',
+  imports: [],
+  templateUrl: './chance-setion.component.html',
+  styleUrl: './chance-setion.component.scss'
+})
+export class ChanceSetionComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "../../components/header/header.component";
 import { DiscoverSectionComponent } from "../../components/discover-section/discover-section.component";
 import { EnjoySectionComponent } from "../../components/enjoy-section/enjoy-section.component";
 import { DifferencesSectionComponent } from "../../components/differences-section/differences-section.component";
+import { ChanceSetionComponent } from "../../components/chance-setion/chance-setion.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, DiscoverSectionComponent, EnjoySectionComponent, DifferencesSectionComponent],
+  imports: [HeaderComponent, DiscoverSectionComponent, EnjoySectionComponent, DifferencesSectionComponent, ChanceSetionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
